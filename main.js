874b13de0c00cc50381d6e0afd06b9913adc8911
sky <you@example.com>
2023-05-06 12:11:18 +0530

    added circle event

--- main.js
+++ main.js
@@ -3,21 +3,39 @@ const canvas = document.querySelector("#canvas");
 
 const ctx = canvas.getContext("2d");
 
+var canvasPos =getPosition(canvas);
+var mouseX = 0;
+var mouseY = 0;
 
-let a= canvas.addEventListener("mousemove", ()=> {
-    
+canvas.addEventListener("mousemove", setMousePosition, false);
+
+function setMousePosition(e)
+{
+    mouseX  = e.clientX - canvasPos.x ;
+
+    mouseY = e.clientY - canvasPos.y;
+}
+
+function drawCircle() {
+  ctx.beginPath();
+  ctx.arc(100, 75, 30, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+drawCircle();
 
 
-    console.log('mouse moving')});
-
-
-   function drawCircle()
-   {
-    ctx.beginPath();
-    ctx.arc(100, 75, 30, 0, 2 * Math.PI);
-    ctx.stroke(); 
-
-   }
-
-
-    drawCircle();
+function getPosition(el) {
+    var xPosition = 0;
+    var yPosition = 0;
+   
+    while (el) {
+      xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
+      el = el.offsetParent;
+    }
+    return {
+      x: xPosition,
+      y: yPosition
+    };
+  }
